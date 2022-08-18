3d125e7a3f615f652e920478ee8f893958f63fc7
alisinancobani <cobanialisinan@gmail.com>
2022-08-18 10:52:08 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => CategoriesScreen(),
-       // CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (context) => MealDetailScreen(),
       },
       onUnknownRoute: (settings) {
